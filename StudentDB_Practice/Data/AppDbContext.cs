ccd5fade26a9b11e60214b4c606b501d9136a399
Marky <markdangal5858@gmail.com>
2026-08-08 07:48:16 +0545

    created data folder and inside that i added the AppDbContext.cs simply i used this beacuse i want to inherit the DbContext and i created the constructor isnide and also i added the DbSet for both catgory and product inside the same file

---
+++ StudentDB_Practice/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentDB_Practice.Models;
+
+namespace StudentDB_Practice.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+          : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
