fca5f1bd523d34c9f2dd8823387ad8519804236f
Marky <markdangal5858@gmail.com>
2026-08-08 11:13:30 +0545

    Created the CategoryController so that we can Get and Post the Http request and resposnse from the browser and server also used the DI

---
+++ StudentDB_Practice/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentDB_Practice.Data;
+using StudentDB_Practice.Models;
+
+namespace StudentDB_Practice.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+
+            return Content($"Category saved! ID = {category.Id}");
+        }
+    }
+
+        
+}
