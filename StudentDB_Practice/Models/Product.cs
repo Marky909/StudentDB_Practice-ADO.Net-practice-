546f4807144978026853144fccbff0d9ef03e7a8
Marky <markdangal5858@gmail.com>
2026-08-08 07:45:58 +0545

    added product model so that it can hold the product id, name ,price and also categoryId as a foerign key and also i created the navigation prop so that the category and product can be connected

---
+++ StudentDB_Practice/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentDB_Practice.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; } //Foreign key 
+        public Category? Category { get; set; } //navigation property
+    }
+}
